ea8f30c28bda539995f03331a9c170b58092e9ff
vsvipul <vsvipul555@gmail.com>
2018-12-12 04:38:36 +0530

    add files

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// setup express app
+const app = express();
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago');
+mongoose.Promise = global.Promise;
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+
+// init routes
+app.use('/api', require('./routes/api'));
+
+// error handling middleware
+app.use(function(err, req, res, next){
+	// console.log(err);
+	res.status(422).send({error: err.message});
+});
+
+// listen for requests
+app.listen(process.env.port || 4000, function(){
+	console.log('listening for requests');
+});
